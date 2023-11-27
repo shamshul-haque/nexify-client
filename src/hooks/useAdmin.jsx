b232c473f6991565a1c448bc43c7312e11482976
shamshul-haque <shamshul.haque.dev@gmail.com>
2023-11-27 14:08:48 +0600

    fixed access issues

--- src/hooks/useAdmin.jsx
+++ src/hooks/useAdmin.jsx
@@ -1,16 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
-import useAxiosPrivate from "./useAxiosPrivate";
+// import useAxiosPrivate from "./useAxiosPrivate";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useAdmin = () => {
-  const axiosPrivate = useAxiosPrivate();
+  // const axiosPrivate = useAxiosPrivate();
+  const axiosPublic = useAxiosPublic();
   const { user } = useAuth();
 
   const { data: isAdmin, isPending } = useQuery({
     queryKey: ["isAdmin", user?.email],
     enabled: !!user?.email,
     queryFn: async () => {
-      const res = await axiosPrivate.get(`users/admin?email=${user?.email}`);
+      const res = await axiosPublic.get(`users/admin?email=${user?.email}`);
       return res?.data?.admin;
     },
   });
--- src/hooks/useModerator.jsx
+++ src/hooks/useModerator.jsx
@@ -1,16 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "./useAuth";
-import useAxiosPrivate from "./useAxiosPrivate";
+// import useAxiosPrivate from "./useAxiosPrivate";
+import useAxiosPublic from "./useAxiosPublic";
 
 const useModerator = () => {
-  const axiosPrivate = useAxiosPrivate();
+  // const axiosPrivate = useAxiosPrivate();
+  const axiosPublic = useAxiosPublic();
   const { user } = useAuth();
 
   const { data: isModerator, isPending } = useQuery({
     queryKey: ["isModerator", user?.email],
     enabled: !!user?.email,
     queryFn: async () => {
-      const res = await axiosPrivate.get(`users/admin?email=${user?.email}`);
+      const res = await axiosPublic.get(`users/admin?email=${user?.email}`);
       return res?.data?.moderator;
     },
   });
